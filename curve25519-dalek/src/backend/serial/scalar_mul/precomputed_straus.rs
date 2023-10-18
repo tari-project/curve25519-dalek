1aedc3e4299664ef97f9020239b4e15000093b10
Aaron Feickert <66188213+AaronFeickert@users.noreply.github.com>
2023-10-18 11:33:31 -0500

    Formatting

--- curve25519-dalek/src/backend/serial/scalar_mul/precomputed_straus.rs
+++ curve25519-dalek/src/backend/serial/scalar_mul/precomputed_straus.rs
@@ -99,7 +99,6 @@ impl VartimePrecomputedMultiscalarMul for VartimePrecomputedStraus {
                 }
             }
 
-
             #[allow(clippy::needless_range_loop)]
             for i in 0..static_nafs.len() {
                 let t_ij = static_nafs[i][j];
